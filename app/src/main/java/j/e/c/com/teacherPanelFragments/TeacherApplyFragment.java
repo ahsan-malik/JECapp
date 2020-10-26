5243be82b84da594fa022f4f3181f84ae5e8fb39
ahsan-malik <malikahsan2001@gmail.com>
2020-10-26 16:26:09 +0500

    commit04

--- app/src/main/java/j/e/c/com/teacherPanelFragments/TeacherApplyFragment.java
+++ app/src/main/java/j/e/c/com/teacherPanelFragments/TeacherApplyFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,14 +20,14 @@ import j.e.c.com.R;
 public class TeacherApplyFragment extends Fragment {
 
 
-   // @BindView(R.id.agentSpinner)
-    //MaterialSpinner agentSpinner;
+    @BindView(R.id.agentSpinner)
+    Spinner agentSpinner;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_teacher_apply, container, false);
-       // ButterKnife.bind(this, view);
+        ButterKnife.bind(this, view);
         return view;
     }
 
@@ -37,8 +39,12 @@ public class TeacherApplyFragment extends Fragment {
     }
 
     void updateSpinners() {
-        String[] agentArray = {"Agent Reference", "JEC1", "JEC2"};
-        //agentSpinner.setItems(agentArray);
-        //agentSpinner.setSelectedIndex(0);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.agentArray, R.layout.spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        agentSpinner.setAdapter(adapter);
     }
 }
