f340928411d9f50f4ddbbcff247c900b4e4ab7b9
ahsan-malik <malikahsan2001@gmail.com>
2020-11-02 17:59:23 +0500

    commit13

--- app/src/main/java/j/e/c/com/teacherPanelFragments/TeacherApplyFragment.java
+++ app/src/main/java/j/e/c/com/teacherPanelFragments/TeacherApplyFragment.java
@@ -1,13 +1,17 @@
 package j.e.c.com.teacherPanelFragments;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,7 +21,18 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.util.List;
 import java.util.Objects;
 
 import butterknife.BindView;
@@ -26,7 +41,13 @@ import butterknife.OnClick;
 import j.e.c.com.Others.Helper;
 import j.e.c.com.R;
 
+import static android.app.Activity.RESULT_OK;
+
 public class TeacherApplyFragment extends Fragment {
+
+    static final int IMAGE_REQUEST_CODE = 100;
+    static final int VIDEO_REQUEST_CODE = 101;
+    static final int CV_REQUEST_CODE = 102;
 
     boolean seflToogle = false;
 
@@ -51,6 +72,10 @@ public class TeacherApplyFragment extends Fragment {
     TextView self;
     @BindView(R.id.agentSpinnerParent)
     TextInputLayout agentSpinnerParent;
+    @BindView(R.id.cvTextView)
+    TextView cvTextView;
+    @BindView(R.id.videoTextView)
+    TextView videoTextView;
 
     @Nullable
     @Override
@@ -67,10 +92,28 @@ public class TeacherApplyFragment extends Fragment {
         updateSpinners();
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null && data.getData() != null) {
+            switch (requestCode) {
+                case IMAGE_REQUEST_CODE:
+                    picture.setImageURI(data.getData());
+                    break;
+                case VIDEO_REQUEST_CODE:
+                    videoTextView.setVisibility(View.VISIBLE);
+                    videoTextView.setText(data.getData().toString());
+                    break;
+                case CV_REQUEST_CODE:
+                    cvTextView.setVisibility(View.VISIBLE);
+                    cvTextView.setText(data.getData().toString());
+                    break;
+            }
+        }
+
+    }
+
     void updateSpinners() {
-
-        //agent spinner
-
         agentSpinner.setAdapter(Helper.getSimpleSpinnerAdapter(R.array.agentArray, getContext()));
     }
 
@@ -81,7 +124,7 @@ public class TeacherApplyFragment extends Fragment {
     }
 
     @SuppressLint("ResourceAsColor")
-    @OnClick({R.id.backArrow, R.id.self})
+    @OnClick({R.id.backArrow, R.id.self, R.id.camera, R.id.uploadVideoBtn, R.id.uploadCVbtn})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.backArrow:
@@ -100,6 +143,84 @@ public class TeacherApplyFragment extends Fragment {
                     seflToogle = !seflToogle;
                 }
                 break;
+            case R.id.camera:
+                openCamera();
+                break;
+            case R.id.uploadVideoBtn:
+                openGalleryForVideo();
+                break;
+            case R.id.uploadCVbtn:
+                getResumeFromPhone();
+                break;
         }
+    }
+
+    private void getResumeFromPhone() {
+        Dexter.withContext(getContext()).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                Intent intent = new Intent();
+                intent.setType("application/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select file"), CV_REQUEST_CODE);
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+            }
+        }).withErrorListener(new PermissionRequestErrorListener() {
+            @Override
+            public void onError(DexterError dexterError) {
+
+            }
+        }).check();
+    }
+
+    private void openGalleryForVideo() {
+        Dexter.withContext(getContext()).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent , VIDEO_REQUEST_CODE);
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+            }
+        }).withErrorListener(new PermissionRequestErrorListener() {
+            @Override
+            public void onError(DexterError dexterError) {
+
+            }
+        }).check();
+    }
+
+    private void openCamera() {
+        Dexter.withContext(getContext()).withPermissions(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE).withListener(new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                if (multiplePermissionsReport.areAllPermissionsGranted()) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, IMAGE_REQUEST_CODE);
+                }
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+
+            }
+        }).withErrorListener(dexterError -> Toast.makeText(getContext(), dexterError.toString(), Toast.LENGTH_SHORT).show()).check();
     }
 }
