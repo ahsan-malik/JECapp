6e3a87a03b758464a3835c6eee563ff45859760f
ahsan-malik <malikahsan2001@gmail.com>
2020-11-04 17:57:13 +0500

    commit16

--- app/src/main/java/j/e/c/com/teacherPanelFragments/DummyListingFragment.java
+++ app/src/main/java/j/e/c/com/teacherPanelFragments/DummyListingFragment.java
@@ -8,16 +8,32 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.ncorti.slidetoact.SlideToActView;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import j.e.c.com.R;
 
 public class DummyListingFragment extends Fragment {
+    @BindView(R.id.lock)
+    SlideToActView lock;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dummy_listing, container, false);
         ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        lock.setOnSlideCompleteListener(slideToActView -> {
+            FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, new TeacherApplyFragment()).addToBackStack(null).commit();
+        });
     }
 }
