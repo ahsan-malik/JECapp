485ed46537c5bf22bba1e3f8224c73e7fc390569
ahsan-malik <malikahsan2001@gmail.com>
2020-12-05 18:01:44 +0500

    commit42

---
+++ app/src/main/java/j/e/c/com/commonFragments/PaymentFragment.java
@@ -0,0 +1,23 @@
+package j.e.c.com.commonFragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import butterknife.ButterKnife;
+import j.e.c.com.R;
+
+public class PaymentFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_payment, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+}
